dd9d8bf4e922a20dba8b6d973e82f5cf182663b0
Yorlan-r <106920094+Yorlan-r@users.noreply.github.com>
2022-09-11 22:34:03 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Arrays can be used to store _____.",
+        answer: "All the above",
+        options: [
+        "Strings",
+        "Booleans",
+        "Numbers",
+        "All the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if/else statement is enlclosed with _______.",
+        answer: "Curly brackets",
+        options: [
+        "square brackets",
+        "Curly brackets",
+        "quotes",
+        "parenthesis"
+        ]
+    },
+    {
+        numb: 3,
+        question: "String values must be enclosed within______ when being assigned to variables.",
+        answer: "quotes",
+        options: [
+        "commas",
+        "curly brackets",
+        "quotes",
+        "parenthesis"
+        ]
+    },
+    {
+        numb: 4,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        answer: "console.log",
+        options: [
+        "terminal/bash",
+        "for loops",
+        "console.log",
+        "Javascript"
+        ]
+    },
+];
